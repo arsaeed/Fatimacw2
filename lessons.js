a1e1d507838c1189536f5c2a4f240237c9d102f6
arsaeed <81184632+arsaeed@users.noreply.github.com>
2021-07-03 15:52:45 -0700

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,91 @@
+let myArr = [
+    
+    {
+      "id": 1002,
+      "title": "chemistry",
+      "description": " All work no play, makes jack a doll boy.",
+      "location":"Alianz studio",
+      "price": 2.99,
+      "image": "images/2.jpg",
+      "availableInventory": 16,
+    
+    },
+    {
+      "id": 1003,
+      "title": "surgery",
+      "description": "The little masterChef.",
+      "location":"Site Campus",
+      "price": 11.00,
+      "image": "images/33.jpg",
+      "availableInventory": 14,
+      
+    },
+    {
+      "id": 1004,
+      "title": "algorithm",
+      "description": "Time to dive in water",
+      "location":"Dubai Marina",
+      "price": 7.99,
+      "image": "images/44.jpg",
+      "availableInventory": 13,
+     
+    },
+    {
+      "id": 1005,
+      "title": "MARTIAL ARTS",
+      "description": "The art of self depence",
+      "location":"Sharja",
+      "price": 49.99,
+      "image": "images/5.jpg",
+      "availableInventory": 12,
+     
+    },
+    {
+      "id": 1006,
+      "title": "MUSIC",
+      "description": "your voice is your gift.",
+      "location":"Internet City",
+      "price": 45.99,
+      "image": "images/6.jpg",
+      "availableInventory": 11,
+    },
+    {
+      "id": 1007,
+      "title": "photosynthesis ",
+      "description": "Learn to connect.",
+      "location":"Myriad",
+      "price": 49.99,
+      "image": "images/7.jpg",
+      "availableInventory": 10,
+    },
+    {
+      "id": 1008,
+      "title": "MATHS",
+      "description": "Solve with practice",
+      "location":"Bostom",
+      "price": 9.99,
+      "image": "images/8.jpg",
+      "availableInventory": 9,
+    },
+    {
+      "id": 1009,
+      "title": " DRAWING",
+      "description": " Color your passion",
+      "location":"BUK",
+      "price": 41.09,
+      "image": "images/9.jpg",
+      "availableInventory": 8,
+    },
+    {
+      "id": 1010,
+      "title": " snake study",
+      "description": "Plant tree save life",
+      "location":"Room 406",
+      "price": 11.11,
+      "image": "images/1010.jpg",
+      "availableInventory": 7,
+    },
+    
+
+
+  ]
